2d422a52798c6854a6bc8a58b80aef7c82146139
naykun <yankun1138283845@foxmail.com>
2024-03-17 05:44:39 -0700

    [fix] 64 node update

--- dataset_dataloading/download.sh
+++ dataset_dataloading/download.sh
@@ -1,12 +1,12 @@
 rank=$NODE_RANK
-echo "META FILE /f_ndata/G/dataset/panda/full_shard/panda70m_training_full_${rank}.csv"
-echo "Saving to /f_ndata/G/dataset/panda/full_shard/part-${rank}"
+echo "META FILE /f_ndata/G/dataset/panda/full_shard_64/panda70m_training_full_${rank}.csv"
+echo "Saving to /f_ndata/G/dataset/panda/full_shard_64/part-${rank}"
 echo "Logging at panda/part${rank}"
-video2dataset --url_list="/f_ndata/G/dataset/panda/full_shard/panda70m_training_full_${rank}.csv" \
+video2dataset --url_list="/f_ndata/G/dataset/panda/full_shard_64/panda70m_training_full_${rank}.csv" \
               --url_col="url" \
               --caption_col="caption" \
               --clip_col="timestamp" \
-              --output_folder="/f_ndata/G/dataset/panda/full_shard/part-${rank}" \
+              --output_folder="/f_ndata/G/dataset/panda/full_shard_64/part-${rank}" \
               --save_additional_columns="[matching_score]" \
               --config="video2dataset/video2dataset/configs/panda_70M.yaml"\
 	      --enable_wandb=True \
